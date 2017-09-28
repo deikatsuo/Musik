3324c1cf1198b4f93f2ff6f7ec7e66d18d9b33a1
Deri herdianto <derryherdianto@gmail.com>
2017-09-29 02:39:26 +0700

    Simple GUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate gtk;
+extern crate gio;
+
+pub mod musik;
+
+use gio::ApplicationExt;
+
 fn main() {
-    println!("Hello, world!");
+    match gtk::Application::new("org.haseup.musik",
+                                gio::APPLICATION_FLAGS_NONE)
+    {
+        Ok(app) => {
+            app.connect_activate(|app| musik::init(app));
+            app.run(&[]);
+        },
+        Err(e)  => {
+            println!("Failed to initialize Gtk: {:?}", e);
+            return;
+        }
+    }
 }
